35081640870c7a31060369ffb2ac0a45e396494a
John Roberts <bigjlr2004@yahoo.com>
2023-08-16 11:42:36 -0500

    Completed Place Names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,25 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
   console.log(name);
+}
+console.log("-----------------");
+console.log("The Place Names");
+for (const name of names) {
+  if (name.startsWith("The")) {
+    console.log(name);
+  }
 }
